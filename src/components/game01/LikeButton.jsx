6af45b40ff9cb6ce37bdcaa9f11e49a125be0c97
jeonnni <wjsdmsal9865@gmail.com>
2025-03-28 23:05:51 +0900

    react-webgame

--- src/components/game01/LikeButton.jsx
+++ src/components/game01/LikeButton.jsx
@@ -3,10 +3,12 @@ import { useState } from "react"
 const LikeButton = () => {
     const [liked, setLiked] = useState(false);
 
+
+    if(liked){
+        return 'You liked this.'
+    }
     return (
-        <button onClick={ () => setLiked(true)} >
-            {liked ? 'You like this.' : 'Like'}
-        </button>
+        <button onClick={ () => setLiked(true)}>Like</button>
     )
 }
 
